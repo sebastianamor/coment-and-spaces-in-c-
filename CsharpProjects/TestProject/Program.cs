cbf06513f5c1578c417cb0017158d0b249424114
sebastian lapt <smoreira11@hotmail.com>
2025-05-08 12:21:39 +0900

    add new file

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,11 +1,13 @@
-﻿// Example 1:
-Console
-.
-WriteLine
-(
-"Hello Example 1!"
-)
-;
-
-// Example 2:
-string firstWord="Hello";string lastWord="Example 2";Console.WriteLine(firstWord+" "+lastWord+"!");
+﻿string str = "The quick brown fox jumps over the lazy dog.";
+// convert the message into a char array
+char[] charMessage = str.ToCharArray();
+// Reverse the chars
+Array.Reverse(charMessage);
+int x = 0;
+// count the o's
+foreach (char i in charMessage) { if (i == 'o') { x++; } }
+// convert it back to a string
+string new_message = new String(charMessage);
+// print it out
+Console.WriteLine(new_message);
+Console.WriteLine($"'o' appears {x} times.");
