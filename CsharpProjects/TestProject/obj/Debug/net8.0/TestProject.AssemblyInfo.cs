b57cd5735cce3fde9fe840b6db65901d5dd3b28b
sebastian lapt <smoreira11@hotmail.com>
2025-05-09 08:05:46 +0900

    add finish

--- CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9028396b255dedb60a8684a6bdac77aac15cea13")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbf06513f5c1578c417cb0017158d0b249424114")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
